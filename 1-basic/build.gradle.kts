5997c860de98ce5afd63b6d18d16ef50ddad84f2
Yuta Ueno <y.ueno0803@gmail.com>
2022-04-02 03:48:04 +0900

    Update build.gradle.kts

--- 1-basic/build.gradle.kts
+++ 1-basic/build.gradle.kts
@@ -23,12 +23,6 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:30.1.1-jre")
-
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
