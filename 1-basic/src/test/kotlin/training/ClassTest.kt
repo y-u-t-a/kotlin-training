2c2deef4815423c28fb12a76054ec6eec8ea0f9b
Yuta Ueno <y.ueno0803@gmail.com>
2022-04-04 21:40:24 +0900

    Update ClassTest.kt

--- 1-basic/src/test/kotlin/training/ClassTest.kt
+++ 1-basic/src/test/kotlin/training/ClassTest.kt
@@ -3,14 +3,20 @@ package training
 import kotlin.test.*
 
 class ClassTest {
+    /**
+     * Class は、インスタンスを値として比較できない
+     */
     @Test
     fun notEqualClass() {
         val u1 = User("User", 20)
         val u2 = User("User", 20)
         assertNotEquals(u1, u2)
-        assertFalse { u1 == u2 }
+        assertTrue { u1 != u2 }
     }
 
+    /**
+     * DataClass は、インスタンスを値として比較できる
+     */
     @Test
     fun dataClass() {
         val u1 = DataClassUser("User", 20)
