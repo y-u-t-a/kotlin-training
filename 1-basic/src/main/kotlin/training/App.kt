ec934fad8eeec184d687cb849b9bd8b5ef6b6a43
Yuta Ueno <y.ueno0803@gmail.com>
2022-04-04 23:26:53 +0900

    Update App.kt

--- 1-basic/src/main/kotlin/training/App.kt
+++ 1-basic/src/main/kotlin/training/App.kt
@@ -6,4 +6,10 @@ package training
  */
 fun main() {
     println("Hello World!")
+
+    val user = User(name = "User", age = 22)
+    println("Name: ${user.name}, Age: ${user.age}")
+
+    val userData = DataClassUser(name = "User", age = 22)
+    println(userData)
 }
