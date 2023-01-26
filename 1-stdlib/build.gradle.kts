a8a3366e90a766ecf7b932f9a55575494f8dd55d
Yuta Ueno <y.ueno0803@gmail.com>
2023-01-27 01:58:28 +0900

    Kotlin のバージョン情報を DRY にする

--- 1-stdlib/build.gradle.kts
+++ 1-stdlib/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-
+    kotlin("jvm")
     application
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,11 @@ include(
     "1-stdlib",
     "2-serialization",
 )
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
+}
