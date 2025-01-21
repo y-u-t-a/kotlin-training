c5c6efe2b0074449028eefae438e608070961d68
Yuta Ueno <y.ueno0803@gmail.com>
2025-01-22 01:15:04 +0900

    Kotlin のバージョンアップ

--- 2-serialization/build.gradle.kts
+++ 2-serialization/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation(platform(kotlin("bom")))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     // Jackson
     val jacksonVersion = "2.14.2"
     implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
